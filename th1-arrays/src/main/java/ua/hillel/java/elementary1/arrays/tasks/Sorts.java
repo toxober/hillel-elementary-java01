363b8d45af84d71e29ca9874adc03443bf23057b
Andrii Minov <andrii.minov@myheritage.com>
2018-12-18 14:18:54 +0200

    Add tests and tasks for the arrays.

---
+++ th1-arrays/src/main/java/ua/hillel/java/elementary1/arrays/tasks/Sorts.java
@@ -0,0 +1,27 @@
+package ua.hillel.java.elementary1.arrays.tasks;
+
+/**
+ * Some sorts of the arrays.
+ */
+public interface Sorts {
+    /**
+     * Bubble sort.
+     *
+     * @param array the array
+     */
+    void bubbleSort(int[] array);
+
+    /**
+     * Min sort.
+     *
+     * @param array the array
+     */
+    void minSort(int[] array);
+
+    /**
+     * Merge sort.
+     *
+     * @param array the array
+     */
+    void mergeSort(int[] array);
+}
--- th1-arrays/src/test/java/ua/hillel/java/elementary1/arrays/SortsTest.java
+++ th1-arrays/src/test/java/ua/hillel/java/elementary1/arrays/SortsTest.java
@@ -1,11 +1,14 @@
 package ua.hillel.java.elementary1.arrays;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import ua.hillel.java.elementary1.arrays.tasks.Sorts;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 @RunWith(Parameterized.class)
@@ -14,10 +17,10 @@ public class SortsTest {
     @Parameterized.Parameters
     public static List<Object[]> parameters() {
         return Arrays.asList(new Object[][]{
-                {new int[] {1, 5, 2, 6, 3, 7, 9, 8, 4}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}},
-                {new int[] {1, 2, 1, 3, 1, 4}, new int[] {1, 1, 1, 2, 3, 4}},
-                {new int[] {1, 1}, new int[] {1, 1}},
-                {new int[] {}, new int[] {}}
+                {new int[]{1, 5, 2, 6, 3, 7, 9, 8, 4}, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+                {new int[]{1, 2, 1, 3, 1, 4}, new int[]{1, 1, 1, 2, 3, 4}},
+                {new int[]{1, 1}, new int[]{1, 1}},
+                {new int[]{}, new int[]{}}
         });
     }
 
@@ -29,24 +32,40 @@ public class SortsTest {
         this.expected = expected;
     }
 
-//    @Test
-//    public void bubbleSort() {
-//        int[] array = Arrays.copyOf(example, example.length);
-//        Sorts.bubbleSort(array);
-//        Assert.assertArrayEquals(array, expected);
-//    }
-//
-//    @Test
-//    public void minSort() {
-//        int[] array = Arrays.copyOf(example, example.length);
-//        Sorts.minSort(array);
-//        Assert.assertArrayEquals(array, expected);
-//    }
-//
-//    @Test
-//    public void mergeSort() {
-//        int[] array = Arrays.copyOf(example, example.length);
-//        Sorts.mergeSort(array);
-//        Assert.assertArrayEquals(array, expected);
-//    }
+    private Collection<Sorts> sorts;
+
+    @Before
+    public void setUp() {
+        sorts = Utils.implementations(Sorts.class);
+    }
+
+    @Test
+    public void bubbleSort() {
+        for (Sorts sort : sorts) {
+            int[] array = Arrays.copyOf(example, example.length);
+            sort.bubbleSort(array);
+            Assert.assertArrayEquals(String.format("Failed %s [%s]", sort.getClass(), Arrays.toString(example)), array,
+                                     expected);
+        }
+    }
+
+    @Test
+    public void minSort() {
+        for (Sorts sort : sorts) {
+            int[] array = Arrays.copyOf(example, example.length);
+            sort.minSort(array);
+            Assert.assertArrayEquals(String.format("Failed %s [%s]", sort.getClass(), Arrays.toString(example)), array,
+                                     expected);
+        }
+    }
+
+    @Test
+    public void mergeSort() {
+        for (Sorts sort : sorts) {
+            int[] array = Arrays.copyOf(example, example.length);
+            sort.mergeSort(array);
+            Assert.assertArrayEquals(String.format("Failed %s [%s]", sort.getClass(), Arrays.toString(example)), array,
+                                     expected);
+        }
+    }
 }
